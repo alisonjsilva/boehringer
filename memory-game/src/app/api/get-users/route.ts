d6121617538cd5233b111c31e453c8a437c31b2e
Alison Silva <alison.silva@wundermanthompson.com>
2023-09-12 23:31:46 +0100

    Update route.ts

--- memory-game/src/app/api/get-users/route.ts
+++ memory-game/src/app/api/get-users/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
 
-    const { rows, fields } = await sql`SELECT * FROM Ranking ORDER BY Moves AND ORDER BY Ranking_date;`
+    const { rows, fields } = await sql`SELECT * FROM Ranking ORDER BY Moves ASC, Ranking_date DESC LIMIT 10;`
     // console.log(rows, fields)
     return NextResponse.json({ rows, fields }, { status: 200 })
 }
